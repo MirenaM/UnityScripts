d61a1387a85ecacb2d62c3c6cc22f8bc9b6b96fc
Mirena <mirena.mclean@gmail.com>
2019-10-16 19:53:48 -0700

    Added Shield Ability

---
+++ Abilities/Shield/Shield.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public bool _IsDissolving { get; private set; }
+
+    [SerializeField]
+    private GameObject _Model = default;
+    private Material _Material;
+
+    private Coroutine _DissolveIn;
+    private Coroutine _DissolveOut;
+
+    [SerializeField]
+    private float _DissolveSpeed = 5f;
+
+    [HideInInspector]
+    public ShieldAbility _ShieldAbility;
+
+    private void Awake()
+    {
+        _Material = _Model.gameObject.GetComponent<Renderer>().material;
+        _Material.SetFloat("_DissolveAmount", 1f);
+    }
+
+    private void OnEnable()
+    {
+        _ShieldAbility.HasShield = true;
+        StartDissolveIn();
+    }
+
+    private void Update()
+    {
+        if (_ShieldAbility is null) return;
+        transform.position = _ShieldAbility.transform.position;
+    }
+
+    public void StartDissolveIn()
+    {
+        _IsDissolving = true;
+
+        if (_DissolveIn != null)
+        {
+            StopCoroutine(_DissolveIn);
+        }
+
+        _Material.SetFloat("_DissolveAmount", 1f);
+
+        _DissolveIn = StartCoroutine(ShieldDissolveIn());
+    }
+
+    private IEnumerator ShieldDissolveIn()
+    {
+        var currentTime = 0f;
+
+        while (currentTime < 1f)
+        {
+            var lerpAmount = Mathf.Lerp(1, 0, currentTime);
+            _Material.SetFloat("_DissolveAmount", lerpAmount);
+            currentTime += Time.deltaTime * _DissolveSpeed;
+            yield return null;
+        }
+
+        _Material.SetFloat("_DissolveAmount", 0f);
+
+
+        _IsDissolving = false;
+        yield return null;
+    }
+
+    public void StartDissolveOut()
+    {
+        _IsDissolving = true;
+
+        if (_DissolveOut != null)
+        {
+            StopCoroutine(_DissolveOut);
+        }
+        if (_DissolveIn != null)
+        {
+            StopCoroutine(_DissolveIn);
+        }
+
+        _Material.SetFloat("_DissolveAmount", 0f);
+
+        _DissolveOut = StartCoroutine(ShieldDissolveOut());
+    }
+
+    private IEnumerator ShieldDissolveOut()
+    {
+        var currentTime = 0f;
+
+        while (currentTime < 1f)
+        {
+            var lerpAmount = Mathf.Lerp(0, 1, currentTime);
+            _Material.SetFloat("_DissolveAmount", lerpAmount);
+            currentTime += Time.deltaTime * _DissolveSpeed;
+            yield return null;
+        }
+
+        _Material.SetFloat("_DissolveAmount", 1f);
+        _IsDissolving = false;
+        gameObject.SetActive(false);
+
+        yield return null;
+    }
+
+    private void OnDisable()
+    {
+        _ShieldAbility.HasShield = false;
+    }
+}
---
+++ Abilities/Shield/ShieldAbility.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class ShieldAbility : MonoBehaviour
+{
+    [SerializeField]
+    private Shield _ShieldPrefab = default;
+    private Shield _CurrentShield;
+
+    public bool HasShield = false;
+
+    [SerializeField]
+    private KeyCode _ShieldKey = default;
+
+    private void Awake()
+    {
+        var GO = Instantiate(_ShieldPrefab.gameObject, transform.position, transform.rotation);
+        _CurrentShield = GO.GetComponent<Shield>();
+        _CurrentShield._ShieldAbility = this;
+
+        _CurrentShield.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        ShieldInputs();
+    }
+
+    private void ShieldInputs()
+    {
+        if (_CurrentShield is null) return;
+        if (HasShield == false && Input.GetKeyDown(_ShieldKey) && _CurrentShield._IsDissolving == false)
+        {
+            Activate();
+        }
+
+        if (Input.GetKeyUp(_ShieldKey) && HasShield == true)
+        {
+            Deactivate();
+        }
+    }
+
+    public void Activate()
+    {
+        _CurrentShield.transform.position = transform.position;
+        _CurrentShield.transform.rotation = transform.rotation;
+        _CurrentShield.gameObject.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        _CurrentShield.GetComponent<Shield>().StartDissolveOut();
+    }
+
+}
