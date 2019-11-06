508b90fec40a3f48192428f96f000c62a36547dd
Mirena <mirena.mclean@gmail.com>
2019-11-06 13:24:27 -0800

    Added FadeMaterial script
    Fades a material out using an opacity property on a shader

---
+++ FadeMaterial.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeMaterial : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _Model = default;
+
+    [SerializeField]
+    private float _FadeTime = 4f;
+
+    [SerializeField]
+    private bool _FadeOnEnable = true;
+
+    private List<Material> _Materials = new List<Material>();
+
+    private void Awake()
+    {
+        var transforms = _Model.gameObject.GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < transforms.Length; i++)
+        {
+
+            var rend = transforms[i].GetComponent<Renderer>();
+
+            if (rend != null)
+            {
+                _Materials.Add(rend.material);
+            }
+
+        }
+
+    }
+
+    private void OnEnable()
+    {
+        if (_FadeOnEnable)
+        {
+            StartFadeOut();
+        }
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < _Materials.Count; i++)
+        {
+            _Materials[i].SetFloat("_Opacity", 1f);
+            _Materials[i].SetFloat("_OutlineAlpha", 1f);
+        }
+    }
+
+    public void StartFadeOut()
+    {
+        for (int i = 0; i < _Materials.Count; i++)
+        {
+            _Materials[i].SetFloat("_Opacity", 1f);
+            _Materials[i].SetFloat("_OutlineAlpha", 1f);
+        }
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float currentTime = 0f;
+
+        while (currentTime < _FadeTime)
+        {
+            float lerpAmount = Mathf.Lerp(1, 0, currentTime);
+            float lerpAmountOutline = Mathf.Lerp(1, 0, currentTime * 2f);
+
+            for (int i = 0; i < _Materials.Count; i++)
+            {
+                _Materials[i].SetFloat("_Opacity", lerpAmount);
+            }
+
+            currentTime += Time.deltaTime / _FadeTime;
+            yield return null;
+        }
+    }
+}
